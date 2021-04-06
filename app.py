37de8c1705eb6cf775802084ae63439c34dfb645
Tumladen <56322716+Tumladen@users.noreply.github.com>
2021-04-06 11:14:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,75 @@ st.line_chart(df.Volume)
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 df1 = scaler.fit_transform((np.array(df1).reshape(-1, 1)))
+# Split
 
+training_size = int(len(df1) * 0.65)
+test_size = len(df1) - training_size
+train_data, test_data = df1[0: training_size, :], df1[training_size: len(df1), :1]
+
+
+# Convert into dataset
+
+
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+
+    for i in range(len(dataset) - time_step - 1):
+        a = dataset[i: (i + time_step), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+
+trained_date = int(user_input["trained_date"])
+X_train, y_train = create_dataset(train_data, trained_date)
+X_test, y_test = create_dataset(test_data, trained_date)
+
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+#LSTM
+model=Sequential()
+model.add(LSTM(50,return_sequences=True,input_shape=(100,1)))
+model.add(LSTM(50,return_sequences=True))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error',optimizer='adam')
+
+model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=int(user_input["num_epoch"]),batch_size=64,verbose=1)
+
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+
+
+last_day = 100
+x_input_range = len(test_data) - last_day
+x_input = test_data[x_input_range:].reshape(1,-1)
+
+temp_input = list(x_input)
+temp_input = temp_input[0].tolist()
+
+
+lst_output = []
+n_steps = last_day
+i = 0
+predict_day = int(user_input["predict_day"])
+
+while(i < predict_day):
+
+    if(len(temp_input) > last_day):
+        x_input = np.array((temp_input[1:]))
+        x_input = x_input.reshape(1, -1)
+        x_input = x_input.reshape((1, n_steps, 1))
+        yhat = model.predict(x_input, verbose=0)
+        temp_input.extend(yhat[0].tolist())
+        temp_input = temp_input[1:]
+        lst_output.extend(yhat.tolist())
+        i = i + 1
+    else:
+        x_input = x_input.reshape((1, n_steps, 1))
+        yhat = model.predict(x_input, verbose=0)
+        temp_input.extend(yhat[0].tolist())
+        lst_output.extend(yhat.tolist())
+        i = i + 1
+st.table(scaler.inverse_transform(lst_output))
