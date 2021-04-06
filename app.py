746c0100355f142c5e1dec9a5e1d8d5adf815e32
Tumladen <56322716+Tumladen@users.noreply.github.com>
2021-04-06 10:36:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
 import datetime
+import pandas as pd
+import datapackage
 
 st.write("This program will never be able to predict the future values exactly. Although, It might give an intuition towards the current and potential future trend of a specific stock.")
 
@@ -11,5 +13,17 @@ min_date = datetime.datetime(2015, 1, 1)
 max_date = datetime.date(2020, 1, 1)
 
 a_date = st.date_input("Pick a date", (min_date, max_date))
+
+start = a_date[0]
+end = a_date[1]
+
+#Companies
+data_url = 'https://datahub.io/core/s-and-p-500-companies/datapackage.json'
+package = datapackage.Package(data_url)
+resources = package.resources
+for resource in resources:
+    if resource.tabular:
+        data = pd.read_csv(resource.descriptor['path'])
+st.write(data)
 
 
