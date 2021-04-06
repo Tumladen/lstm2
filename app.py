33ffb16c5b1f2f3e8a5ef43025d2b9c4611c6c74
Tumladen <56322716+Tumladen@users.noreply.github.com>
2021-04-06 10:21:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 
 import streamlit as st
+import datetime
 
-st.header('Welcome')
+st.write("This program will never be able to predict the future values exactly. Although, It might give an intuition towards the current and potential future trend of a specific stock.")
 
+# Date
+st.title("Date range")
 
+min_date = datetime.datetime(2015, 1, 1)
+max_date = datetime.date(2020, 1, 1)
+
+a_date = st.date_input("Pick a date", (min_date, max_date))
 
 
